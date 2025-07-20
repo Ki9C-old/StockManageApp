fa8fdfac654039b87d336c9effa32ce2864e88ab
Ki9C-old <ri9.jp313@gmail.com>
2025-07-20 22:24:42 +0900

    F5リロード対策
    Signed-off-by: Ki9C-old <ri9.jp313@gmail.com>

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    historyApiFallback: true,
     watch: {
       usePolling: true,  // ファイル変更監視をポーリングに切り替え
       interval: 100      // 監視間隔（ミリ秒）
